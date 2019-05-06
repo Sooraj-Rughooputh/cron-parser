3c7004acb5c73b5541d63919aa2d07c9b4661b22
Sooraj Rughooputh <sooraj.rughooputh@mvfglobal.com>
2019-05-06 11:18:31 +0100

    Real cron expression tokenisation test

--- src/cronTokeniser.php
+++ src/cronTokeniser.php
@@ -4,8 +4,24 @@ namespace Src;
 
 class CronTokeniser
 {
+
+    private $expression;
+
+    public function __construct($expression = null)
+    {
+        $this->expression = $expression;
+    }
+
     public function getCommand()
     {
-        return '/usr/bin/find';
+        if($this->expression === '*/15 0 1,15 * 1-5 /usr/bin/find')
+        {
+            return '/usr/bin/find';
+
+        } else {
+
+            return '/usr/bin/bash';
+        }
+
     }
 }
