7f4cd39661a6427b9b3886a214020c884e0d1a15
Sooraj Rughooputh <sooraj.rughooputh@mvfglobal.com>
2019-05-06 14:05:04 +0100

    updated formatter to output header columnwith a width of 15

--- src/CronFormatter.php
+++ src/CronFormatter.php
@@ -4,6 +4,8 @@ namespace Src;
 
 class CronFormatter
 {
+    const HEADING_COLUMN_WIDTH = 15;
+
     private $lines = [];
 
     public function addLine($heading, $data)
@@ -36,7 +38,7 @@ class CronFormatter
 
     private function renderHeading($heading)
     {
-        return $heading . str_repeat(' ', 14 - strlen($heading));
+        return $heading . str_repeat(' ', static::HEADING_COLUMN_WIDTH - strlen($heading));
     }
 
     private function reset()
