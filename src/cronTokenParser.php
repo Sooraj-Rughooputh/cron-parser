b10d88ba73d913c388894e4b58140af0e27f4f74
Sooraj Rughooputh <sooraj.rughooputh@mvfglobal.com>
2019-05-06 11:26:35 +0100

    make cron token parser test work

--- src/cronTokenParser.php
+++ src/cronTokenParser.php
@@ -30,19 +30,69 @@ class CronTokenParser
     private function parseTokensToValues()
     {
         foreach ($this->tokens ?? [] as $token) {
-            $this->values += $this->parseTokenToValues($token);
+            $this->values = array_merge($this->values, $this->parseTokenToValues($token));
         }
     }
 
     private function parseTokenToValues($token)
     {
-        return
-          $this->parseWildcardTokenToValues($token)
-          +
-          $this->parseRangeTokenToValues($token)
-          +
+        return array_merge(
+          $this->parseWildcardTokenToValues($token),
+          $this->parseRangeTokenToValues($token),
           $this->parseBasicTokenToValues($token)
-          ;
+        );
+    }
+
+    private function parseWildcardTokenToValues($token)
+    {
+        if (!preg_match('/^\*\/(\d+)$/', $token, $matches)) {
+            return [];
+        }
+
+        $denominator = $matches[1];
+
+        return array_filter(
+          $this->validRange,
+          function($candidateValue) use ($denominator)
+          {
+              return ($candidateValue % $denominator) == 0;
+          }
+        );
+    }
+
+    private function parseRangeTokenToValues($token)
+    {
+        if (!preg_match('/^(\d+)\-(\d+)$/', $token, $matches)) {
+            return [];
+        }
+
+        $firstInRange = $matches[1];
+        $lastInRange = $matches[2];
+
+        return array_filter(
+          $this->validRange,
+          function($candidateValue) use ($firstInRange, $lastInRange)
+          {
+              return $candidateValue >= $firstInRange && $candidateValue <= $lastInRange;
+          }
+        );
+    }
+
+    private function parseBasicTokenToValues($token)
+    {
+        if (!preg_match('/^(\d+)$/', $token, $matches)) {
+            return [];
+        }
+
+        $basicValue = $matches[1];
+
+        return array_filter(
+          $this->validRange,
+          function($candidateValue) use ($basicValue)
+          {
+              return $candidateValue == $basicValue;
+          }
+        );
     }
 
 }
